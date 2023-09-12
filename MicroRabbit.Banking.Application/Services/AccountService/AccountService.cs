ff787ce0fbcb0be0c44ba967774f7fd9677af847
unknown <tanedochrisjohn@gmail.com>
2023-09-12 12:18:37 +0800

    Add Transfer Command

--- MicroRabbit.Banking.Application/Services/AccountService/AccountService.cs
+++ MicroRabbit.Banking.Application/Services/AccountService/AccountService.cs
@@ -1,5 +1,7 @@
 ﻿using MicroRabbit.Banking.Data.Repository.AccountRepository;
+using MicroRabbit.Banking.Domain.Commands;
 using MicroRabbit.Banking.Domain.Models;
+using MicroRabbit.Domain.Core.Bus;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,10 +13,12 @@ namespace MicroRabbit.Banking.Application.Services.AccountService
     public class AccountService : IAccountService
     {
         private readonly IAccountRepository _accountRepository;
+        private readonly IEventBus _eventBus;
 
-        public AccountService(IAccountRepository accountRepository)
+        public AccountService(IAccountRepository accountRepository, IEventBus eventBus)
         {
             _accountRepository = accountRepository;
+            _eventBus = eventBus;
         }
 
         public async Task<IEnumerable<Account>> GetAllAccount()
@@ -31,6 +35,19 @@ namespace MicroRabbit.Banking.Application.Services.AccountService
         {
              await _accountRepository.Create(account);
             return account;
+        }
+
+       
+
+        public void TransferFunds(AccountTransfer accountTransfer)
+        {
+            var createTransferCommand = new CreateTransferCommand(
+                    accountTransfer.FromAccount,
+                    accountTransfer.ToAccount,
+                    accountTransfer.TranferAmount
+                );
+
+            _eventBus.SendCommand(createTransferCommand);
         }
     }
 }
---
+++ MicroRabbit.Banking.Domain/CommandHandlers/TransferCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using MicroRabbit.Banking.Domain.Commands;
+using MicroRabbit.Domain.Core.Bus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroRabbit.Banking.Domain.CommandHandlers
+{
+    public class TransferCommandHandler : IRequestHandler<CreateTransferCommand, bool>
+    {
+        private readonly IEventBus _eventBus;
+
+        public TransferCommandHandler(IEventBus eventBus)
+        {
+            _eventBus = eventBus;
+        }
+        public Task<bool> Handle(CreateTransferCommand request, CancellationToken cancellationToken)
+        {
+
+            return Task.FromResult(true);
+        }
+    }
+}
